2799ca106b4d85b74e102d8aa98f3c6edc157f6c
Durga Prasad Komma (V) <dkomma@apple.com>
2016-11-30 00:32:56 -0800

    first change done directly in master

--- SampleApps/src/main/java/com/durga/FirstClass.java
+++ SampleApps/src/main/java/com/durga/FirstClass.java
@@ -3,13 +3,7 @@ package com.durga;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		//Updated by Durga, further updated by mamatha
-		//More chages by durga
-		//Changes done by durga in Durga_Branch
-		
-		//Changes done by durga in durga branch while ther are some udpate files already commited into main branch
-
-		//more changes by durga again
+		// first change done directly in master
 	}
 
 }
