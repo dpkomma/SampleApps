0ddd2d7bfcb107029f3b6a53262426a9cfe59042
Durga Prasad Komma (V) <dkomma@apple.com>
2016-11-30 01:21:51 -0800

    third changes in master directly

--- SampleApps/src/main/java/com/durga/FirstClass.java
+++ SampleApps/src/main/java/com/durga/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// first change done directly in master
 		//second change  done directly in master
+		//Third changes in master directly
 	}
 
 }
