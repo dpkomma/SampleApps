15119c281e344aa08db9b32c5f68052c9838da28
Durga Prasad Komma (V) <dkomma@apple.com>
2016-12-10 11:44:03 -0800

    master file change and common file change

--- SampleApps/src/main/java/com/durga/CommonFile.java
+++ SampleApps/src/main/java/com/durga/CommonFile.java
@@ -2,4 +2,9 @@ package com.durga;
 
 public class CommonFile {
 //commong file created by both branches
+
+	public void masterMethod(){
+		
+	}
+	
 }
--- SampleApps/src/main/java/com/durga/Master.java
+++ SampleApps/src/main/java/com/durga/Master.java
@@ -2,4 +2,5 @@ package com.durga;
 
 public class Master {
 	//new file created in master branch
+	// first change in master file
 }
